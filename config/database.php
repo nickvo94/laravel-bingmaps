74052a5fc9bf1bf6ae33ed448b160f955d3cda24
perseus0832 <nickvo2308@gmail.com>
2020-05-04 15:18:21 +0300

    change conf file

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-
+$herokuDb = parse_url(env('DATABASE_URL', "postgres://kaennybzwwwpdh:4ac522e994a8dd6613f858909ec534c533521a4894e5723930e4e3a0c918724e@ec2-34-195-169-25.compute-1.amazonaws.com:5432/d4e7ncbdp0j6ia"));
 return [
 
     /*
@@ -63,17 +63,13 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host'     => $herokuDb['host'],
+            'database' => substr($herokuDb['path'], 1),
+            'username' => $herokuDb['user'],
+            'password' => $herokuDb['pass'],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
